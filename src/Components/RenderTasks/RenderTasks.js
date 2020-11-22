b230238a42b37c88bf726584649956dc4f5a0852
yanawishnya <you@example.com>
2020-11-22 03:56:13 +0300

    Домашнее задание №5

--- src/Components/RenderTasks/RenderTasks.js
+++ src/Components/RenderTasks/RenderTasks.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import styles from "./RenderTasks.module.scss"
+import classnames from 'classnames/bind'
+
+const cx = classnames.bind(styles)
 
 class Task extends React.Component {
     render () {
         return (
-            <div  className="task">
-                <p>Name: {this.props.task.name}</p>
-                <p>Description: {this.props.task.description}</p>
-                <p>Completed: {this.props.task.completed.toString()}</p>
-                <button className="btn" onClick={this.props.changeStatus}>{this.props.task.message}</button>
+            <div  className={cx("task-blocks", {[`task-blocks-theme-${this.props.theme}`]: true})}>
+                <p className={cx("task-name", {[`task-name-theme-${this.props.theme}`]: true})}>Name: {this.props.task.name}</p>
+                <p className={cx("task-description", {[`task-description-theme-${this.props.theme}`]: true})}>Description: {this.props.task.description}</p>
+                <p className={cx("task-completeness", {[`task-completeness-theme-${this.props.theme}`]: true})}>Completed: {this.props.task.completed.toString()}</p>
+                <button onClick={this.props.changeStatus}
+                        className={cx("btn-complete", {[`btn-complete-theme-${this.props.theme}`]: true})}>{this.props.task.message}</button>
             </div>
         )
     }
